18af95addffa9ef0bc1396f56dc7a3f86d4f5784
4x8Matrix <4x8Matrix@Gmail.com>
2022-04-28 19:34:36 +0100

    Spaces -> Tabs

--- sources/FileSystem.lua
+++ sources/FileSystem.lua
@@ -1,8 +1,8 @@
 --[[
-    FileSystem.lua
+	FileSystem.lua
 
-    @Author: AsynchronousMatrix
-    @Licence: ...
+	@Author: AsynchronousMatrix
+	@Licence: ...
 
 	Module used to simplify importing modules
 ]]--
@@ -12,39 +12,39 @@ local FileSystem = { Name = "FileSystem" }
 
 -- // Functions
 function FileSystem.SafeRequire(Module, ...)
-    local Success, Result = pcall(require, Module)
+	local Success, Result = pcall(require, Module)
 
-    if Success then
-        return (type(Result) == "function" and Result(...)) or Result
-    else
-        return nil, Result
-    end
+	if Success then
+		return (type(Result) == "function" and Result(...)) or Result
+	else
+		return nil, Result
+	end
 end
 
 function FileSystem.LoadTable(Source, ...)
-    local Result = { }
+	local Result = { }
 
-    for _, ModuleObject in ipairs(Source) do
-        local Result, Message = FileSystem.SafeRequire(ModuleObject, ...)
+	for _, ModuleObject in ipairs(Source) do
+		local Result, Message = FileSystem.SafeRequire(ModuleObject, ...)
 
-        if not Result and Message then
-            warn(Message)
-        else
-            table.insert(Result, FileSystem.SafeRequire(ModuleObject, ...))
-        end
-    end
+		if not Result and Message then
+			warn(Message)
+		else
+			table.insert(Result, FileSystem.SafeRequire(ModuleObject, ...))
+		end
+	end
 
-    return Result
+	return Result
 end
 
 function FileSystem.LoadChildren(Source, ...)
-    local Result = { }
+	local Result = { }
 
-    for _, ModuleObject in ipairs(Source:GetChildren()) do
-        Result[ModuleObject.Name] = FileSystem.SafeRequire(ModuleObject, ...)
-    end
+	for _, ModuleObject in ipairs(Source:GetChildren()) do
+		Result[ModuleObject.Name] = FileSystem.SafeRequire(ModuleObject, ...)
+	end
 
-    return Result
+	return Result
 end
 
 function FileSystem.LoadTableInto(Source, Table, ...)
@@ -53,18 +53,18 @@ function FileSystem.LoadTableInto(Source, Table, ...)
 			continue
 		end
 
-        Table[ModuleObject.Name] = FileSystem.SafeRequire(ModuleObject, ...)
-    end
+		Table[ModuleObject.Name] = FileSystem.SafeRequire(ModuleObject, ...)
+	end
 end
 
 function FileSystem.LoadChildrenInto(Source, Table, ...)
-    for _, ModuleObject in ipairs(Source:GetChildren()) do
+	for _, ModuleObject in ipairs(Source:GetChildren()) do
 		if ModuleObject == script then
 			continue
 		end
 
-        Table[ModuleObject.Name] = FileSystem.SafeRequire(ModuleObject, ...)
-    end
+		Table[ModuleObject.Name] = FileSystem.SafeRequire(ModuleObject, ...)
+	end
 end
 
 -- // Module
